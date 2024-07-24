7110f67af2a611f228eb55c1d028a770deacd53f
lllllan <342310798@qq.com>
2024-07-24 13:21:54 +0800

    ✨ feat(iam.auth): 增加令牌相关错误码

--- iam_auth.go
+++ iam_auth.go
@@ -3,6 +3,9 @@ package ecodes
 // iam.auth 10.00.xx
 
 const (
-	IAM_USERNAME_NOT_FOUND = 100000 // 用户名不存在
-	IAM_PASSWORD_ERROR     = 100001 // 密码错误
+	IAM_USERNAME_NOT_FOUND                  = 100000 // 用户名不存在
+	IAM_PASSWORD_ERROR                      = 100001 // 密码错误
+	IAM_AUTHENTICATION_TOKEN_NOT_PROVIDED   = 100002 // 认证令牌未提供
+	IAM_AUTHENTICATION_TOKEN_INVALID_FORMAT = 100003 // 无效的认证令牌格式：header 中的 Authorization 必须为 Bearer xxxxxxx
+	IAM_AUTHENTICATION_TOKEN_INVALID        = 100004 // 无效的认证令牌：令牌格式正确但是内容无效或过期
 )
