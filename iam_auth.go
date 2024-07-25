191f2005c414343c16e59470f5a50d7483621963
lllllan <342310798@qq.com>
2024-07-25 23:21:23 +0800

    ✨ feat(iam.auth): 增加用户名校验的相关错误码

--- iam_auth.go
+++ iam_auth.go
@@ -8,4 +8,7 @@ const (
 	IAM_AUTHENTICATION_TOKEN_NOT_PROVIDED   = 100002 // 认证令牌未提供
 	IAM_AUTHENTICATION_TOKEN_INVALID_FORMAT = 100003 // 无效的认证令牌格式：header 中的 Authorization 必须为 Bearer xxxxxxx
 	IAM_AUTHENTICATION_TOKEN_INVALID        = 100004 // 无效的认证令牌：令牌格式正确但是内容无效或过期
+	IAM_INVALID_USERNAME_LENGTH             = 100005 // 用户名长度不合法：需要在 1 到 30 之间
+	IAM_INVALID_USERNAME_FORMAT             = 100006 // 用户名格式不合法：只能包含字母、数字、下划线
+	IAM_USERNAME_ALREADY_EXISTS             = 100007 // 用户名已存在
 )
